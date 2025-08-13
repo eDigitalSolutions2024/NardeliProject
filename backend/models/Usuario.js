044e8b2a07cb94b547802a21afa73bc8e2bcf79f
EduardoEscarzaga-215024 <al215024@alumnos.uacj.mx>
2025-08-13 11:39:23 -0600

    rep

--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -2,19 +2,21 @@ const mongoose = require('mongoose');
 
 const UsuarioSchema = new mongoose.Schema({
   fullname: { type: String },
-  email: { type: String, required: true, unique: true },
-  password: { type: String }, // requerido solo para login tradicional
-  googleId: { type: String }, // usado solo para login con Google
+  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+  password: { type: String },   // login tradicional (si lo usas)
+  googleId: { type: String },   // login con Google
   foto: { type: String },
   role: { type: String, enum: ['admin', 'user'], default: 'user' },
 
+  // Enlace mágico
   emailVerified: { type: Boolean, default: true },
-  magicTokenHasj: {type: String, default: null},
-  magicTokenExo: { type: Date, defualt: null},
+  magicTokenHash: { type: String },  // sin default: null
+  magicTokenExp: { type: Date },     // sin default: null
 
-  lasLoginAt: { tupe: Date, default: null }
-}, { timestamps: true});
+  // ¡ojo con la 't'!
+  lastLoginAt: { type: Date }        // sin default: null
+}, { timestamps: true });
 
-UsuariosSchema.index({ email: 1 }, { unique: true });
+UsuarioSchema.index({ email: 1 }, { unique: true });
 
 module.exports = mongoose.model('Usuario', UsuarioSchema);
